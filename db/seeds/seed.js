0700abc1b73a33f98d4b946bcf80ac049427f457
jakedheath123 <jakedheath@gmail.com>
2020-03-10 17:48:26 +0000

    remove unnecessary console log

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -8,7 +8,6 @@ const {
 const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
 exports.seed = function(knex) {
-  console.log("help me");
   return knex.migrate
     .rollback()
     .then(function() {
