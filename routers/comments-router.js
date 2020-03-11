b4826045ea7672c8419b72d20390fe86d7fe9eff
jakedheath123 <jakedheath@gmail.com>
2020-03-11 18:34:41 +0000

    Added delete method to comments router

--- routers/comments-router.js
+++ routers/comments-router.js
@@ -1,10 +1,14 @@
 const commentsRouter = require("express").Router();
-const { patchCommentById } = require("../controllers/comments-controller");
+const {
+  patchCommentById,
+  deleteCommentById
+} = require("../controllers/comments-controller");
 const { handle405Errors } = require("../errors/index");
 
 commentsRouter
   .route("/:comment_id")
   .patch(patchCommentById)
+  .delete(deleteCommentById)
   .all(handle405Errors);
 
 module.exports = commentsRouter;
