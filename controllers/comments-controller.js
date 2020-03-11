dab528957aefc09b368b0541ed64fa3e466868a7
jakedheath123 <jakedheath@gmail.com>
2020-03-11 18:35:50 +0000

    Added function to delete comment by id function

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,4 +1,7 @@
-const { updateCommentById } = require("../models/comments-model");
+const {
+  updateCommentById,
+  removeCommentById
+} = require("../models/comments-model");
 
 exports.patchCommentById = function(request, response, next) {
   const { comment_id } = request.params;
@@ -7,6 +10,18 @@ exports.patchCommentById = function(request, response, next) {
   updateCommentById(comment_id, inc_votes)
     .then(function(comment) {
       response.status(200).send({ comment });
+    })
+    .catch(function(error) {
+      next(error);
+    });
+};
+
+exports.deleteCommentById = function(request, response, next) {
+  const { comment_id } = request.params;
+
+  removeCommentById(comment_id)
+    .then(function(comment) {
+      response.status(204).send({ comment });
     })
     .catch(function(error) {
       next(error);
