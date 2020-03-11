ff73e1df658c107847b9259c3714d1b26cb49048
jakedheath123 <jakedheath@gmail.com>
2020-03-11 18:32:46 +0000

    Added 405 error handler to api route for incorrect methods

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,6 +3,9 @@ const topicsRouter = require("./topics-router");
 const usersRouter = require("./users-router");
 const articlesRouter = require("./articles-router");
 const commentsRouter = require("./comments-router");
+const { handle405Errors } = require("../errors/index");
+
+apiRouter.route("/").all(handle405Errors);
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
