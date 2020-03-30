784ebb8c0f235cd2875e63a2c9d69063475bef1a
jakedheath123 <jakedheath@gmail.com>
2020-03-30 16:57:57 +0100

    added function to get route for GET /api in api router

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,12 +3,12 @@ const topicsRouter = require("./topics-router");
 const usersRouter = require("./users-router");
 const articlesRouter = require("./articles-router");
 const commentsRouter = require("./comments-router");
+const { sendAllEndpoints } = require("../controllers/api-controller");
 const { handle405Errors } = require("../errors/index");
-const { jsonEndpoints } = require("./json-router");
 
 apiRouter
   .route("/")
-  //.get(jsonEndpoints)
+  .get(sendAllEndpoints)
   .all(handle405Errors);
 
 apiRouter.use("/topics", topicsRouter);
