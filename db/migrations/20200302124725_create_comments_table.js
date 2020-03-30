a70eb27fcfd45768959effc335915c899fdd7a59
jakedheath123 <jakedheath@gmail.com>
2020-03-30 16:12:04 +0100

    added CASCADE Delete all comments when the article they are related to is deleted

--- db/migrations/20200302124725_create_comments_table.js
+++ db/migrations/20200302124725_create_comments_table.js
@@ -5,7 +5,10 @@ exports.up = function(knex) {
       .string("author")
       .references("users.username")
       .notNullable();
-    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable
+      .integer("article_id")
+      .references("articles.article_id")
+      .onDelete("CASCADE");
     commentsTable.integer("votes").defaultTo(0);
     commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
     commentsTable.text("body").notNullable();
